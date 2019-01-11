830176456ca36c9c3328ab0f466ee5e87bdec291
Hazel Brookes <hmb1@hw.ac.uk>
2019-01-11 16:24:27 +0000

    Update

--- js/navbar.js
+++ js/navbar.js
@@ -3,5 +3,7 @@ window.onscroll = function () {
     "use strict";
     if (document.body.scrollTop >= 200 ) {
         myNav.classList.toggle("nav-colored");
-    } 
+    } else{
+		myNav.classList.remove("nav-colored");
+	}
 };
