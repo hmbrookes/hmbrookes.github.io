4b3134f80756c82a398dd6a4d13d30ba0989dd16
hmbrookes <46491861+hmbrookes@users.noreply.github.com>
2019-01-13 22:42:01 +0000

    Update JS

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,7 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
+window.onscroll = function() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("navbar").className = "nav-colored";
   } else {
     document.getElementById("navbar").className = "";
   }
-}
+};
